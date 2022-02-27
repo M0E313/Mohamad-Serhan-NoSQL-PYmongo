aeeda039241497086648e335ca67714352de4890
code_moe <75614655+M0E313@users.noreply.github.com>
2022-02-27 23:12:10 +0100

    Update 4.py

--- 4.py
+++ 4.py
@@ -1,5 +1,5 @@
 
-##4
+##4 Send back the fourth countries of a continent by alphabetic order
 
 continents = continents.find({},{'Name':1, 'countries':{'$slice':4}}).sort("Name")
     for FourCountry in continents:
