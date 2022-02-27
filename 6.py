8bd52ff1770e99864f9824dbec526cef6a84a345
code_moe <75614655+M0E313@users.noreply.github.com>
2022-02-27 23:13:19 +0100

    Update 6.py

--- 6.py
+++ 6.py
@@ -1,4 +1,4 @@
-##6
+##6 Get all the countries order by number of people first the less populated and last the most populated
 
     for countries in col.find({}).sort("Population"):
         print(countries['Name'], countries['Population'])
